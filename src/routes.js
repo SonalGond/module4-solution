521c71dade4bf207cbfbee942ce1edb2befbbf8b
Sonal Gond <sonal@cour-sera.com>
2018-10-13 22:24:24 +0530

    Latest Version

---
+++ src/routes.js
@@ -0,0 +1,55 @@
+(function () {
+'use strict';
+
+angular.module('MenuApp')
+.config(RoutesConfig);
+
+RoutesConfig.$inject = ['$stateProvider', '$urlRouterProvider'];
+function RoutesConfig($stateProvider, $urlRouterProvider) {
+
+  // Redirect to home page if no other URL matches
+  $urlRouterProvider.otherwise('/');
+
+  // *** Set up UI states ***
+  $stateProvider
+
+  // Home page
+  .state('home', {
+    url: '/',
+    templateUrl: 'src/templates/home.template.html'
+  })
+
+  // Categories
+  .state('categories', {
+    url: '/categories',
+    templateUrl: 'src/templates/mainCategories.template.html',
+    controller: 'CategoriesController as categoriesCtrl',
+    resolve: {
+      categories: ['MenuDataService', function (MenuDataService) {
+        return MenuDataService.getAllCategories().then(function(result){
+          return result.data;
+        });
+      }]
+    }
+  })
+
+   //items
+   .state('items', {
+    url: '/items/{category}',
+    templateUrl: 'src/templates/mainItem.template.html',
+    controller: 'ItemsController as itemsCtrl',
+    resolve: {
+      items: ['$stateParams', 'MenuDataService',
+            function ($stateParams, MenuDataService) {
+                return MenuDataService.getItemsForCategories($stateParams.category)
+                .then(function (items) {
+                  return items.data.menu_items;
+                });
+            }]
+    }
+  });
+
+
+}
+
+})();
